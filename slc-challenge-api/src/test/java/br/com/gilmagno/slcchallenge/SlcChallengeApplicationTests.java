27bd9c95480fd9769eeb01b992b2c015e4ba96b9
Gilmagno Costa <gilmagno.costa@gmail.com>
2019-11-13 08:41:15 -0200

    Implementado classe de teste unitario

--- slc-challenge-api/src/test/java/br/com/gilmagno/slcchallenge/SlcChallengeApplicationTests.java
+++ slc-challenge-api/src/test/java/br/com/gilmagno/slcchallenge/SlcChallengeApplicationTests.java
@@ -1,13 +1,46 @@
 package br.com.gilmagno.slcchallenge;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import br.com.gilmagno.slcchallenge.entities.BcMsgEntity;
+import br.com.gilmagno.slcchallenge.entities.SisMsgEntity;
+import br.com.gilmagno.slcchallenge.entities.SlcEntity;
+import br.com.gilmagno.slcchallenge.exceptions.SlcServiceException;
+import br.com.gilmagno.slcchallenge.repositories.SlcRepository;
+import br.com.gilmagno.slcchallenge.services.SlcServiceImpl;
+
+/**
+ * Testes unitarios da classe SlcService
+ * utilizando JUnit e Mockito
+ * @author Gilmagno Costa
+ *
+ */
 @SpringBootTest
 class SlcChallengeApplicationTests {
 
+	@Mock
+	private SlcRepository slcRepositoryMock;
+	
+	@InjectMocks
+	private SlcServiceImpl slcService;
+		
 	@Test
-	void contextLoads() {
+	void shouldGetSlcData() throws SlcServiceException {
+		
+		SlcEntity slcEntityExpected = new SlcEntity();
+		slcEntityExpected.setBcMsg(new BcMsgEntity());
+		slcEntityExpected.setSisMsg(new SisMsgEntity());
+		
+		Mockito.when(slcRepositoryMock.save(Mockito.any())).thenReturn(slcEntityExpected);
+		SlcEntity slc = slcService.getSlcData();
+		
+		assertTrue(slc != null);
 	}
 
 }
